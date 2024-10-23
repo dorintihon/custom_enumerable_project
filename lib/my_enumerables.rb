094c95983abc1fdccb508ef66c4115d55767a03d
Dorin <gimiri99@gmail.com>
2024-10-23 15:27:07 -0400

    finished my_none? definition

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,62 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    for element in self
+      yield element, index
+      index += 1
+    end
+  end
+
+  def my_select
+      arr = []
+      for element in self
+        arr << element if yield element
+      end
+
+      return arr
+  end
+
+  def my_all?
+    bool = false
+    values = 0
+    elements = 0
+    for element in self
+      elements += 1
+      values +=1 if yield element
+    end
+    if elements == values
+      return true
+    end
+    return bool
+  end
+
+  def my_any?
+    bool = false
+    for element in self
+      if yield element
+         return true
+      end
+    end
+    return bool
+  end
+
+  def my_none?
+    bool = true
+    values = 0
+    elements = 0
+    for element in self
+      if yield element
+        return false
+      end
+      elements += 1
+      values +=1 if yield element
+    end
+    if elements == values
+      return false
+    end
+    return bool
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +65,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+      for element in self
+        yield element
+      end
+  end
 end
